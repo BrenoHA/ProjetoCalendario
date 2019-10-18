d8b6e393b0a29ccf464eca1d51b66d0f55c7cf33
MozanielPSC <56358038+MozanielPSC@users.noreply.github.com>
2019-10-18 18:06:04 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 require('./models/db');
 const express = require ('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
 const employeeController = require('./controllers/employeeController');
 var app = express();
+app.set('views',path.join(__dirname,'/views/'));
+app.engine('hbs',exphbs({extname:'hbs',defaultlayout:'mainLayout',layoutsDir:__dirname + '/views/layouts/'}))
+app.set('view engine','hbs');
+app.use('/employee',employeeController);
 app.listen(3000,()=>{
 console.log('Express server started at port : 3000');
 });
-app.use('/employee',employeeController);
