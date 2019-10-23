e5080cd95e325e11c4509c9d2d418e37e91fee04
jvictormleao <56364911+jvictormleao@users.noreply.github.com>
2019-10-22 23:34:19 -0300

    agenda

--- Projeto/controller/agendamentoController.js
+++ Projeto/controller/agendamentoController.js
@@ -14,14 +14,11 @@ router.post('/agendar', async (req, res) => {
 	}
 });
 
-router.get('/agenda', async (req, res) => {
+router.get('/agenda', (req, res) => {
+  Agenda.find().then((agendamento) => {
+    res.render('./agenda', { agendamento });
 
-	const agendados = await Agenda.find();
-
-	//res.send({ agendados });
-
-	res.render('agenda', {agendamento: agendados});
-
+});
 });
 
 
